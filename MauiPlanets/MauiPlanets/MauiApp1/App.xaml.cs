5f70296adee3e719307f3b513c77541d1bc38432
AliikaPSP <aliika.puusepp@hotmail.com>
2024-09-10 11:44:36 +0300

    PlanetsScreenSize

--- MauiPlanets/MauiPlanets/MauiApp1/App.xaml.cs
+++ MauiPlanets/MauiPlanets/MauiApp1/App.xaml.cs
@@ -1,6 +1,9 @@
-﻿using MauiApp1.Views;
+﻿#if WINDOWS
 using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Windows.Graphics;
+#endif
+using MauiApp1.Views;
 
 namespace MauiApp1
 {
@@ -12,18 +15,19 @@ namespace MauiApp1
         {
             InitializeComponent();
 
-            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
-            {
-    #if WINDOWS
-                var mauiWindow = handler.VirtualView;
-                var nativeWindow = handler.PlatformView;
-                nativeWindow.Activate();
-                IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
-                WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
-                AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-                appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
-    #endif
-            });
+#if WINDOWS
+                Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+                {
+                    var mauiWindow = handler.VirtualView;
+                    var nativeWindow = handler.PlatformView;
+                    nativeWindow.Activate();
+                    IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                    WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+                    AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                    appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+                });
+#endif
+           
 
             MainPage = new StartPage();
         }
