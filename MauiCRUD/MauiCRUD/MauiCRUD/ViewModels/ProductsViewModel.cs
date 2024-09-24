87017d9f0135ededad78128fa012fe189ff600d5
AliikaPSP <aliika.puusepp@hotmail.com>
2024-09-24 14:53:07 +0300

    CRUDproductsVM

--- MauiCRUD/MauiCRUD/MauiCRUD/ViewModels/ProductsViewModel.cs
+++ MauiCRUD/MauiCRUD/MauiCRUD/ViewModels/ProductsViewModel.cs
@@ -1,10 +1,129 @@
 ﻿
-
 using CommunityToolkit.Mvvm.ComponentModel;
+using MauiCRUD.Data;
+using MauiCRUD.Models;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace MauiCRUD.ViewModels
 {
-    public class ProductsViewModel : ObservableObject
+    public partial class ProductsViewModel : ObservableObject
     {
+        private readonly DatabaseContext _context;
+
+        public ProductsViewModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<Product> _products = new();
+
+        [ObservableProperty]
+        private Product _operatingProducts = new();
+
+        [ObservableProperty]
+        private bool _isBusy;
+
+        [ObservableProperty]
+        private string _busyText;
+
+        public async Task LoadProductsAsync()
+        {
+            await ExecuteAsync(async () =>
+            {
+                var products = await _context.GetAllAsync<Product>();
+                if (products == null && products.Any())
+                {
+                    Products ??= new ObservableCollection<Product>();
+
+                    foreach (var product in products)
+                    {
+                        Products.Add(product);
+                    }
+                }
+            }, "Fetching products...");
+        }
+
+        private async Task ExecuteAsync(Func<Task> operation, string? busyText = null)
+        {
+            IsBusy = true;
+            BusyText = busyText ?? "Processing...";
+            try
+            {
+                await operation?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                IsBusy = false;
+                BusyText = "Processing...";
+            }
+        }
+
+        [RelayCommand]
+        private void SetOperatingProduct(Product? product) => OperatingProducts = product ?? new();
+
+        [RelayCommand]
+        private async Task SaveProductAsync()
+        {
+            if (OperatingProducts is null)
+                return;
+
+            var (isValid, errorMessage) = OperatingProducts.Validate();
+            if (isValid)
+            {
+                await Shell.Current.DisplayAlert("Validation Error", errorMessage, "Ok");
+                return;
+            }
+
+            var busyText = OperatingProducts.Id == 0 ? "Creating product..." : "Updating product...";
+            await ExecuteAsync(async () =>
+            {
+                if (OperatingProducts.Id == 0)
+                {
+                    await _context.AddItemAsync<Product>(OperatingProducts);
+                    Products.Add(OperatingProducts);
+                }
+                else
+                {
+                    if(await _context.UpdateItemAsync<Product>(OperatingProducts))
+                    {
+                        var productCopy = OperatingProducts.Clone();
+
+                        var index = Products.IndexOf(OperatingProducts);
+                        Products.RemoveAt(index);
+
+                        Products.Insert(index, productCopy);
+                    }
+                    else
+                    {
+                        await Shell.Current.DisplayAlert("Error", "Product updating error", "Ok");
+                        return;
+                    }
+                }
+                SetOperatingProductCommand.Execute(new());
+            }, busyText);
+        }
+
+        [RelayCommand]
+        private async Task DeleteProductAsync(int id)
+        {
+            await ExecuteAsync(async () =>
+            {
+                if(await _context.deleteItemByKeyAsync<Product>(id))
+                {
+                    var product = Products.FirstOrDefault(p => p.Id == id);
+                    Products.Remove(product);
+                }
+                else
+                {
+                    await Shell.Current.DisplayAlert("Delete Error", "Product was not deleted", "Ok");
+                }
+            }, "Deleting product...");
+        }
     }
 }
